065ec85773d94c5c1a34c1bdd1bbd2d45f90e6cf
Phillip <phir@itu.dk>
2026-02-02 11:12:10 +0100

    - Changed the structure of the parser a bit. We should now be able to parse both the outer shell, the inner (description and exercise).

--- src/app/hooks/parser.ts
+++ src/app/hooks/parser.ts
@@ -34,4 +34,76 @@ type ExerciseType =
   | "select"
   | "calculate"
 
+type StepParse = {
+  step: Step;
+  currentI: number;
+}
+
+function parse(input: string, currentLineNumber: number): Code {
+  const lines: string[] = input.split("\n");
+  let code: Code = {
+    information: { name: "", participants: []},
+    step: []
+  }
+
+  let i: number = currentLineNumber;
+
+  while (i < lines.length) {
+    const line: string | undefined = lines[i]?.trim()
+
+    if (line == undefined) {
+      throw new Error(`Line ${line} - Line is undefined`);
+    }
+
+    if (line.startsWith("protocol:")) {
+      code.information.name = line.replace("protocol:", "").trim();
+    }
+    else if (line.startsWith("participants:")) {
+      code.information.participants =
+        line.replace("participants:", "")
+          .split(",")
+          .map(p => p.trim())
+    }
+    else if (line.startsWith("step")) {
+      let result = stepParse(lines, i);
+      i = result.currentI
+      code.step.push(result.step)
+    }
+    else {
+      throw new Error(`Line ${i} - Unexpected string: '${line}'`);
+    }
+    i++
+  }
+
+  return code;
+}
+
+function stepParse(lines: string[], currentLineNumber: number): StepParse {
+  let i: number = currentLineNumber;
+
+  let currentStep: StepParse = { step: { description: ""}, currentI: i}
+
+  while (i < lines.length) {
+    const line: string | undefined = lines[i]?.trim()
+
+    if (line == undefined) {
+      throw new Error(`Line ${line} - Line is undefined`);
+    }
+
+    if (line.startsWith("description:")) {
+      currentStep.step.description = line.replace("description:", "").trim()
+    }
+    else if (line.startsWith("exercise:")) {
+      line.replace("exercise:", "")
+    }
+    else {
+      currentStep.currentI = i
+      return currentStep
+    }
+    i++
+  }
+  currentStep.currentI = i
+  return currentStep;
+}
+
 
