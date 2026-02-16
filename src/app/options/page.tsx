5155aecc729b50c762f9717d287f5b10026435a2
Phillip <phir@itu.dk>
2026-02-16 22:18:58 +0100

    Fixed compilation error

--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -1,19 +1,27 @@
 import SelectExercise from "~/app/_components/exercises/selectExercise/select";
-
+import type { Expr } from "~/app/hooks/parser";
 
 export default function OptionsPage() {
+  const options: Expr[] = [
+    { kind: "var", name: "A" },
+    { kind: "var", name: "B" },
+    { kind: "var", name: "C" },
+    { kind: "var", name: "D" },
+  ];
+
+  const answers: Expr[] = [
+    { kind: "var", name: "B" },
+  ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-pattern p-8">
       <SelectExercise
-        numberOfOptions={4}
-        options={["Option A", "Option B", "Option C", "Option D"]}
-        description={"Bob computes his public key B"}
-        prompt={"Which expression does Bob use to calculate his public key B"}
-        hint={"This is an example of a hint"}
-        answers={[]}
+        options={options}
+        description="Bob computes his public key B"
+        prompt="Which expression does Bob use to calculate his public key B"
+        hint="This is an example of a hint"
+        answers={answers}
       />
-
     </main>
   );
 }
